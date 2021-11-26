d86eb49c10759284fb3a7c77409d3bdae6d2673f
Peterson <peter-larson@hotmail.com>
2021-11-26 17:06:18 -0300

    Desafio 2

--- src/useCases/createDish/CreateDishController.ts
+++ src/useCases/createDish/CreateDishController.ts
@@ -5,16 +5,15 @@ import { CreateDishUseCase } from "./CreateDishUseCase";
 class CreateDishController {
   async handle(req: Request, res: Response): Promise<Response> {
 
-    /* 
-    @TODO - 
-      pegar o id do restaurante dos parâmetros de rota
-      pegar o nome e o preço do corpo da requisição
-    */
-
+    const { name, price } = req.body;
+    const { restaurantId } = req.params;
+    if(!name || !price || !restaurantId) {
+      return res.sendStatus(400)
+    }
     const createDishUseCase = new CreateDishUseCase();
-    // const dish = await createDishUseCase.execute({ name, price, restaurantId });
+    const dish = await createDishUseCase.execute({ name, price, restaurantId });
 
-    // return res.status(201).json(dish);
+    return res.status(201).json(dish);
   }
 }
 
